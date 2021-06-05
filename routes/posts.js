99d41f03a51a377fe1a85a450cb437c7f165c5da
BPitts8019 <bradleyj.pitts@gmail.com>
2021-06-05 13:54:49 -0700

    Create POST /api/posts endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -4,9 +4,7 @@ const postsDb = require("../data/db");
 const router = express.Router();
 
 /*
-
 Method	Endpoint	Description
-POST	/api/posts	Creates a post using the information sent inside the request body.
 POST	/api/posts/:id/comments	Creates a comment for the post with the specified id using information sent inside of the request body.
 GET	/api/posts/:id	Returns the post object with the specified id.
 GET	/api/posts/:id/comments	Returns an array of all the comment objects associated with the post with the specified id.
@@ -14,28 +12,39 @@ DELETE	/api/posts/:id	Removes the post with the specified id and returns the del
 PUT	/api/posts/:id	Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
 
 
-GET	/api/posts	Returns an array of all the post objects contained in the database.
-When the client makes a GET request to /api/posts:
 
-find(): calling find returns a promise that resolves to an array of all the posts contained in the database.
-
-If there's an error in retrieving the posts from the database:
-cancel the request.
-respond with HTTP status code 500.
-return the following JSON object: { error: "The posts information could not be retrieved." }.
 */
+router.post("/", async (req, res) => {
+   if (!req.body.title || !req.body.contents) {
+      res.status(400).json({
+         errorMessage: "Please provide title and contents for the post.",
+      });
+      return;
+   }
+
+   try {
+      const { id } = await postsDb.insert({
+         title: req.body.title,
+         contents: req.body.contents,
+      });
+      const [newPost] = await postsDb.findById(id);
+      res.status(201).json(newPost);
+   } catch (error) {
+      res.status(500).json({
+         error: "There was an error while saving the post to the database",
+      });
+   }
+});
 
 router.get("/", async (req, res) => {
    try {
-      const data = await postsDb.find();
-      res.json({ data });
+      const posts = await postsDb.find();
+      res.json(posts);
    } catch (error) {
       res.status(500).json({
          error: "The posts information could not be retrieved.",
       });
    }
-
-   res.json({ message: "it's working" });
 });
 
 module.exports = router;
