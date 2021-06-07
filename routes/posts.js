7f2608bc3d5c427644eccd03b0ceef4cfd212168
BPitts8019 <bradleyj.pitts@gmail.com>
2021-06-06 23:49:50 -0700

    Create PUT /api/posts/:id endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,6 @@ Method	Endpoint	Description
 POST	/api/posts/:id/comments	Creates a comment for the post with the specified id using information sent inside of the request body.
 GET	/api/posts/:id/comments	Returns an array of all the comment objects associated with the post with the specified id.
 DELETE	/api/posts/:id	Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
-PUT	/api/posts/:id	Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
 */
 router.post("/", async (req, res) => {
    if (!req.body.title || !req.body.contents) {
@@ -59,6 +58,40 @@ router.get("/:id", async (req, res) => {
    } catch (error) {
       res.status(500).json({
          error: "The post information could not be retrieved.",
+      });
+   }
+});
+
+router.put("/:id", async (req, res) => {
+   const { id } = req.params;
+   const { title, contents } = req.body;
+
+   if (!title || !contents) {
+      res.status(400).json({
+         message: "Please provide title and contents for the post.",
+      });
+      return;
+   }
+
+   try {
+      const [post] = await postsDb.findById(id);
+      if (!post) {
+         res.status(404).json({
+            message: "The post with the specified ID does not exist.",
+         });
+         return;
+      }
+
+      await postsDb.update(id, {
+         title,
+         contents,
+      });
+
+      const [updatedPost] = await postsDb.findById(id);
+      res.json(updatedPost);
+   } catch (error) {
+      res.status(500).json({
+         error: "The post information could not be modified.",
       });
    }
 });
